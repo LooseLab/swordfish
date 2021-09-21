46b90315bb8a7f1c3cd6d782147e7417003871c0
adoni5 <roryjmunro1@gmail.com>
2021-09-21 11:30:55 +0100

    Add in swordifying numerous slugs, get _coords endpoint, validating task in steps

--- src/swordfish/endpoints.py
+++ src/swordfish/endpoints.py
@@ -4,10 +4,11 @@ from enum import Enum
 class EndPoint(Enum):
     SWORDFISH_BASE = "/readfish/swordfish"
     RUNS = "/reads/runs/{}/"
-    VALIDATE_TASK = "/{}/"
-    TEST = "/minknow/test-connect/"
+    VALIDATE_TASK = "/{}/validate/{}"
+    TEST = "/test-connect/"
+    GET_COORDS = "/{}/chopchop"
 
-    def swordify_url(self, run_id):
+    def swordify_url(self, run_id="", second_slug=""):
         """
         Swordfishify urls, formatting the provided run id into them
         Returns
@@ -15,7 +16,10 @@ class EndPoint(Enum):
         run_id: str
             The run id UUID
         """
+        # todo switch to list kwargs.values for formatting
+        if second_slug:
+            return f"{self.__class__.SWORDFISH_BASE}{self.value.format(run_id, second_slug)}"
         return f"{self.__class__.SWORDFISH_BASE}{self.value.format(run_id)}"
 
     def __str__(self):
-        return "{self.value}"
+        return f"{self.value}"
