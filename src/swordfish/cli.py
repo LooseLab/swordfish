e1554f709fd0487e51e0745108d0e469faeb08a5
adoni5 <roryjmunro1@gmail.com>
2022-02-01 16:16:51 +0000

    Add options for breakpoints. move all shared args to before the subparser

--- src/swordfish/cli.py
+++ src/swordfish/cli.py
@@ -19,38 +19,44 @@ parser = argparse.ArgumentParser(description="swordfish app")
 
 subparsers = parser.add_subparsers(dest='subparser_name', title='subcommands', help='additional help')
 
+parser_breakpoints = subparsers.add_parser("breakpoints", help="Connect to minoTour,"
+                                                               " use adaptive sampling to try to narrow down"
+                                                               " putative CNVs.")
+parser_breakpoints.set_defaults(func=monitor)
 parser_balance = subparsers.add_parser("balance", help="Connect to minoTour and configure a balancing experiment.")
 parser_balance.set_defaults(func=monitor)
-parser_balance.add_argument(
+parser.add_argument(
     "--mt-key", default=None, help="Access token for MinoTour", required=True
 )
-parser_balance.add_argument(
+parser.add_argument(
     "--mk-host", default="localhost", help="Address for connecting to MinKNOW",
 )
-parser_balance.add_argument(
+parser.add_argument(
     "--mk-port", default=9501, help="Port for connecting to MinKNOW",
 )
-parser_balance.add_argument(
+parser.add_argument(
     "--use_tls", action="store_true", help="Use TLS for connecting to MinKNOW",
 )
-parser_balance.add_argument(
+parser.add_argument(
     "-f",
     "--freq",
     default=DEFAULT_FREQ,
     type=int,
     help=f"Frequency, in seconds, to poll MinoTour, default: {DEFAULT_FREQ}. Cannot be less than {DEFAULT_FREQ}",
 )
-parser_balance.add_argument(
+parser.add_argument(
     "--mt-host",
     default="localhost",
     help="Address for connecting to minoTour. Default - localhost",
 )
-parser_balance.add_argument(
+parser.add_argument(
     "--mt-port",
     default="8100",
     help="Port for connecting to minotour. Default - 8100.",
 )
-parser_balance.add_argument(
+parser.add_argument("--device", type=str, required=True, help="MinION device or GridION position")
+parser.add_argument("--toml", type=Path, required=True, help="Path to TOML file that will be updated")
+parser.add_argument(
     "--no-minknow",
     default=False,
     help="For testing - skips minknow validation. Not recommended."
@@ -63,12 +69,28 @@ parser_balance.add_argument(
     type=int,
     help="Threshold X coverage to start unblocking amplicons on a barcode. Default 50. Cannot be less than 20.",
 )
-parser_balance.add_argument("--toml", type=Path, required=True, help="Path to TOML file that will be updated")
-parser_balance.add_argument("--device", type=str, required=True, help="MinION device or GridION position")
+parser_breakpoints.add_argument(
+    "--min-diff",
+    default=4,
+    type=int,
+    help="Ruptures parameter, for calculating potential breakpoints"
+)
+parser_breakpoints.add_argument(
+    "--exp-ploidy",
+    default=2,
+    type=int,
+    help="Expected ploidy of the species being sequenced. Default 100."
+)
+parser_breakpoints.add_argument(
+    "--reads-bin",
+    default=100,
+    type=int,
+    help="Expected estimated number of reads to be in a bin. Default 100."
+)
 
 
 def signal_handler(signal, frame):
-    print(" Caught Ctrl+C, exiting.", file=sys.stderr)
+    print("Caught Ctrl+C, exiting.", file=sys.stderr)
     sys.exit(0)
 
 
